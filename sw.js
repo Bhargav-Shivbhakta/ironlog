4effbf707e80cb1dee839553dfb02c220aba9fa0
BUS <bhargavshivbhakta@gmail.com>
2026-09-15 08:36:36 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fit-hub-v2';
+const CACHE_NAME = 'fit-hub-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
